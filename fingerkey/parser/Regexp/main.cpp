5af515d3d3ad23c319d0b5b3644241e25ca3a6f9
mdaughtrey <matt@daughtrey.com>
2025-01-21 23:16:46 -0500

    dev

---
+++ fingerkey/parser/Regexp/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Regexp.h"
+#include "parser.h"
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    if (2 != argc)
+    {
+        cerr << "Usage: exec filename" << endl;
+        return 1;
+    }
+    ifstream infile(argv[1]);
+    if (!infile.is_open())
+    {
+        cerr << "Cannot open file " << argv[1] << endl;
+        return 1;
+    }
+    string line;
+    while (getline(infile, line))
+    {
+        cout << line << endl;
+    }
+    infile.close();
+    cout << "Done" << endl;
+    return 0;
+}
